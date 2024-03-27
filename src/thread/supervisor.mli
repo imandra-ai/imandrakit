7c932eab12489440dbaaeee5141cc8c9a6965fcd
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-03-27 09:43:27 -0400

    format

--- src/thread/supervisor.mli
+++ src/thread/supervisor.mli
@@ -1,7 +1,11 @@
-
 (** Supervisor-like features *)
 
-val retry_loop : ?max:int -> ?initial_delay_before_restart_s: float -> ?max_delay_before_restart_s:float -> (unit -> 'a) -> 'a
+val retry_loop :
+  ?max:int ->
+  ?initial_delay_before_restart_s:float ->
+  ?max_delay_before_restart_s:float ->
+  (unit -> 'a) ->
+  'a
 (** [retry_loop f] runs [f()]; if [f()] fails, the loop waits
     a bit and tries again.
     @param max_delay_before_restart_s upper bound on the delay. Default 60s.
