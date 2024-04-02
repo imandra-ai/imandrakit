0120873739c493e5737f9fb941bc7db35776100f
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-04-02 15:44:16 -0400

    test

--- test/core/t_util_zlib.ml
+++ test/core/t_util_zlib.ml
@@ -18,3 +18,17 @@ let () =
   in
   assert_equal ~printer:(spf "%S") s s2;
   true
+
+let () =
+  t ~name:"zip_slice1" @@ fun () ->
+  let s = Bytes.create 4385 in
+  Bytes.iteri (fun i _ -> Bytes.set s i (Char.chr @@ Random.int 255)) s;
+  let s = Bytes.unsafe_to_string s in
+
+  let s1 = Util_zlib.compress_str s in
+  let s2 =
+    Util_zlib.decompress_slice (Bytes.unsafe_of_string s1) 0 (String.length s1)
+    |> Bytes.unsafe_to_string
+  in
+  assert_equal ~printer:(spf "%S") s s2;
+  true
