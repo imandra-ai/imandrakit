91b78f949709a999c41968f5135c2e01d0eeab5e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-04-24 14:03:24 -0400

    add `?frac_s` to `Timestamp_s.to_string_rfc3339`

--- src/core/timestamp_s.ml
+++ src/core/timestamp_s.ml
@@ -5,13 +5,13 @@ type t = (float[@printer Util.pp_duration_s]) [@@deriving show, eq, serpack]
 
 let now : unit -> t = Util.ptime_now
 
-let to_string_rfc3339 ?tz_offset_s (self : t) : string =
+let to_string_rfc3339 ?frac_s ?tz_offset_s (self : t) : string =
   let tz_offset_s =
     match tz_offset_s with
     | Some _ as r -> r
     | None -> Ptime_clock.current_tz_offset_s ()
   in
-  Ptime.to_rfc3339 ?tz_offset_s
+  Ptime.to_rfc3339 ?frac_s ?tz_offset_s
     (Ptime.of_float_s self |> Option.value ~default:(Ptime_clock.now ()))
 
 let of_string_rfc3339 (s : string) : (t, [ `Msg of string ]) result =
