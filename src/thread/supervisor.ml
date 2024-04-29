8fd94a861e6df8b1dd424bbe48ec119d97d21519
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-04-29 12:50:48 -0400

    fix supervisor: make sure we increment count 😅

--- src/thread/supervisor.ml
+++ src/thread/supervisor.ml
@@ -19,9 +19,11 @@ let retry_loop (type a) ?(max = 10) ?(initial_delay_before_restart_s = 0.001)
       | exception e ->
         let bt = Printexc.get_raw_backtrace () in
         let err = Error.of_exn ~bt ~kind:Error_kind.generic_internal_error e in
+
+        incr count;
         if !count < max then
           Log.err (fun k ->
-              k "task failed (retry %d/%d):@ %a" !count max Error.pp err)
+              k "task failed (attempt %d/%d):@ %a" !count max Error.pp err)
         else
           (* exit loop with error *)
           Error.raise_err err;
