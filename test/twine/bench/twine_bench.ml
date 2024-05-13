12bb5079e5fe446422708f1f79c29cfef46edecb
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-05-13 14:27:36 -0400

    bench for twine

--- test/twine/bench/twine_bench.ml
+++ test/twine/bench/twine_bench.ml
@@ -1,5 +1,7 @@
 [@@@ocaml.warning "-32"]
 
+let spf = Printf.sprintf
+
 type term = {
   t_id: int;
   t_view: term_view;
@@ -85,67 +87,38 @@ let rec gen_proof st i : proof =
     MP (lhs, rhs)
   )
 
-let psize p : int =
-  let tbl = Hashtbl.create 16 in
-  let n = ref 0 in
-  let rec loop p =
-    if not (Hashtbl.mem tbl p.p_id) then (
-      incr n;
-      match p.p_view with
-      | MP (a, b) ->
-        loop a;
-        loop b
-      | Assume _ | Refl _ -> ()
-    )
-  in
-  loop p;
-  !n
-
 (* XXX: disable this for normal testing, to be deterministic *)
 let enable_time = ref true
 
-let timeit what f =
+let timeit ?(n = 1) what f =
   let t = Unix.gettimeofday () in
   let res = f () in
-  if !enable_time then
-    Format.printf "time for %s: %.3fs@." what (Unix.gettimeofday () -. t);
+  if !enable_time then (
+    let elapsed = Unix.gettimeofday () -. t in
+    Format.printf "time for %s: %.3fs (%.3fs/iter)@." what elapsed
+      (elapsed /. float n)
+  );
   res
 
-let st = Random.State.make [| 42 |]
-let p = gen_proof st 20
-let () = Format.printf "size(p): %d@." (psize p)
-
-let p_twine =
-  timeit "twine" @@ fun () -> Imandrakit_twine.Encode.to_string proof_to_twine p
-
-(* XXX: full dump, for debug *)
-let enable_dump = ref false
+let make_twine () =
+  let st = Random.State.make [| 42 |] in
+  let p = gen_proof st 20 in
+  Imandrakit_twine.Encode.to_string proof_to_twine p
 
 let () =
-  if !enable_dump then
-    Format.printf "dump:@.%s@." (Imandrakit_twine.Dump.dump_string p_twine)
-
-let () = Format.printf "twine(p): %d B@." (String.length p_twine)
-
-let p_serpack =
-  timeit "serpack" @@ fun () ->
-  Imandrakit_ser_pack.to_cbor_string proof_to_serpack p
-
-let () = Format.printf "serpack(p): %d B@." (String.length p_serpack)
-let p_marshal = timeit "marshal" @@ fun () -> Marshal.to_string p []
-let () = Format.printf "marshal(p): %d B@." (String.length p_marshal)
-
-(* decoding *)
-
-let p2 =
-  timeit "decode_twine" @@ fun () ->
-  Imandrakit_twine.Decode.decode_string proof_of_twine p_twine
-
-let (_ : proof) =
-  timeit "decode_serpack" @@ fun () ->
-  Imandrakit_ser_pack.of_cbor_string_exn proof_of_serpack p_serpack
-
-let (_ : proof) =
-  timeit "decode_marshall" @@ fun () -> Marshal.from_string p_marshal 0
-
-let () = Format.printf "size(p2): %d@." (psize p2)
+  match Sys.argv.(1) with
+  | "gen" ->
+    let s = timeit "twine" @@ fun () -> make_twine () in
+    CCIO.File.write_exn "data.twine" s
+  | "parse" ->
+    let s = CCIO.File.read_exn "data.twine" in
+    Gc.full_major ();
+    let n = 20 in
+    timeit ~n (spf "decoding %d times" n) @@ fun () ->
+    for _i = 1 to n do
+      let (_ : proof) =
+        Imandrakit_twine.Decode.decode_string proof_of_twine s
+      in
+      ()
+    done
+  | _ -> failwith "expect parse|gen"
