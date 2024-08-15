ca1d75a8419833c4986d1d701d1b425b213ef69c
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-08-15 13:02:29 -0400

    detail: tests for twine

--- test/twine/t10.ml
+++ test/twine/t10.ml
@@ -1,9 +1,18 @@
+type bar =
+  | Bar1
+  | Bar2
+[@@deriving eq, show, twine]
+
 type foo =
   | A
-  | B of { x: int }
+  | B of {
+      x: int;
+      y: bar list;
+      z: string option;
+    }
 [@@deriving twine, eq, show]
 
-let x = B { x = 42 }
+let x = B { x = 42; y = [ Bar1 ]; z = Some "a" }
 let x_str = Imandrakit_twine.Encode.encode_to_string foo_to_twine x
 let x' = Imandrakit_twine.Decode.decode_string foo_of_twine x_str
 
