f90951fedfafb5b66e9fa51ef28bd5cdfe7dde17
Christoph M. Wintersteiger <christoph@imandra.ai>
2024-08-27 12:55:28 +0100

    Fixes

--- src/io/setrlimit/libimandrakit_setrlimit_stubs.c
+++ src/io/setrlimit/libimandrakit_setrlimit_stubs.c
@@ -18,11 +18,11 @@ CAMLprim value caml_imandrakit_setrlimit(value _res, value _value) {
   unsigned long limit = Int_val(_value);
 
   struct rlimit old_limits;
-  if (getrlimit(resource, &old_limits) == 0)
+  if (getrlimit(resource, &old_limits) != 0)
     CAMLreturn(false);
 
   const struct rlimit new_limits = {
-      .rlim_cur = limit < old_limits.rlim_cur ?  limit : old_limits.rlim_cur,
+      .rlim_cur = limit < old_limits.rlim_max ?  limit : old_limits.rlim_max,
       .rlim_max = old_limits.rlim_max,
   };
 
