eb5808c259288d6529a5b98c98b0b41aaff4b0d6
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-09-30 12:18:06 -0400

    refactor: trace_async: unify the API for recordings exceptions

--- src/log/trace_async.ml
+++ src/log/trace_async.ml
@@ -8,13 +8,13 @@ let k_parent_scope : Trace.explicit_span Hmap.key = Hmap.Key.create ()
 let[@inline] set_parent_scope (sp : Trace.explicit_span) =
   LS.set_in_local_hmap k_parent_scope sp
 
-let add_exn_to_span (sp : Trace.explicit_span) (exn : exn) =
+let add_exn_to_span (sp : Trace.explicit_span) (exn : exn)
+    (bt : Printexc.raw_backtrace) =
   Trace.add_data_to_manual_span sp
-    [ "exception.message", `String (Printexc.to_string exn) ]
-
-let add_bt_to_span (sp : Trace.explicit_span) (bt : Printexc.raw_backtrace) =
-  Trace.add_data_to_manual_span sp
-    [ "exception.stacktrace", `String (Printexc.raw_backtrace_to_string bt) ]
+    [
+      "exception.message", `String (Printexc.to_string exn);
+      "exception.stacktrace", `String (Printexc.raw_backtrace_to_string bt);
+    ]
 
 open struct
   let with_span_real_ ~level ?data ?__FUNCTION__ ~__FILE__ ~__LINE__ name f =
@@ -47,8 +47,7 @@ open struct
       x
     with e ->
       let bt = Printexc.get_raw_backtrace () in
-      add_exn_to_span span e;
-      add_bt_to_span span bt;
+      add_exn_to_span span e bt;
       Printexc.raise_with_backtrace e bt
 end
 
