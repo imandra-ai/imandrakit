4969635cb01da2753da43ae6b266a6127e762191
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-10-02 21:01:04 -0400

    helpers for Duration_s.t

--- src/core/duration_s.ml
+++ src/core/duration_s.ml
@@ -1,3 +1,11 @@
 (** Duration, in seconds *)
 
 type t = (float[@printer Util.pp_duration_s]) [@@deriving show, serpack, twine]
+
+let ns : t = 1e-9
+let us : t = 1e-6
+let ms : t = 1e-3
+let seconds : t = 1.
+let minutes : t = 60.
+let hours : t = 60. *. minutes
+let days : t = 24. *. hours
