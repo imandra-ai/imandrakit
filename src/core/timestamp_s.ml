4a05625a34ff6d9269a6e6cebe09e4cff90adbbf
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-10-11 13:50:29 -0400

    feat: make timestamp_s yojson-compatible

--- src/core/timestamp_s.ml
+++ src/core/timestamp_s.ml
@@ -19,3 +19,12 @@ let of_string_rfc3339 (s : string) : (t, [ `Msg of string ]) result =
   match Ptime.of_rfc3339 s |> Ptime.rfc3339_error_to_msg with
   | Ok (t, _, _) -> Ok (Ptime.to_float_s t)
   | Error _ as err -> err
+
+let to_yojson self = `String (to_string_rfc3339 self)
+
+let of_yojson = function
+  | `String s ->
+    (match of_string_rfc3339 s with
+    | Ok t -> Ok t
+    | Error (`Msg msg) -> Error msg)
+  | _ -> Error "expexted rfc3339 timestamp"
