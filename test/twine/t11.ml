1de75c87ae861be1dc8941d4fd00d36bcb1739fe
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-01-13 23:19:37 -0500

    test for ref

---
+++ test/twine/t11.ml
@@ -0,0 +1,27 @@
+type bar =
+  | Bar1
+  | Bar2
+[@@deriving eq, show, twine]
+
+type event =
+  | E_bar of bar
+  | E_prev of Imandrakit_twine.offset
+[@@deriving show { with_path = false }, twine]
+
+type foo = { evs: event list } [@@deriving show { with_path = false }, twine]
+
+let encoded : string =
+  let enc = Imandrakit_twine.Encode.create () in
+  let ev0 =
+    Imandrakit_twine.Encode.write_or_ref_immediate enc
+    @@ event_to_twine enc (E_bar Bar1)
+  in
+  let ev1 =
+    Imandrakit_twine.Encode.write_or_ref_immediate enc
+    @@ event_to_twine enc (E_bar Bar2)
+  in
+  let top = foo_to_twine enc { evs = [ E_prev ev0; E_prev ev1 ] } in
+  Imandrakit_twine.Encode.finalize_copy enc ~entrypoint:top
+
+let () =
+  Format.printf "serialized:@.%s@." @@ Imandrakit_twine.Dump.dump_string encoded
