84988ef36a5611e35e8cd4dbda5cd73f7e265a34
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-02-17 22:52:08 -0500

    update tests

--- test/twine/t11.ml
+++ test/twine/t11.ml
@@ -5,7 +5,7 @@ type bar =
 
 type event =
   | E_bar of bar
-  | E_prev of Imandrakit_twine.offset
+  | E_prev of event Imandrakit_twine.offset_for
 [@@deriving show { with_path = false }, twine]
 
 type foo = { evs: event list } [@@deriving show { with_path = false }, twine]
@@ -13,12 +13,10 @@ type foo = { evs: event list } [@@deriving show { with_path = false }, twine]
 let encoded : string =
   let enc = Imandrakit_twine.Encode.create () in
   let ev0 =
-    Imandrakit_twine.Encode.write_or_ref_immediate enc
-    @@ event_to_twine enc (E_bar Bar1)
+    Imandrakit_twine.Encode.write_offset_for enc event_to_twine (E_bar Bar1)
   in
   let ev1 =
-    Imandrakit_twine.Encode.write_or_ref_immediate enc
-    @@ event_to_twine enc (E_bar Bar2)
+    Imandrakit_twine.Encode.write_offset_for enc event_to_twine (E_bar Bar2)
   in
   let top = foo_to_twine enc { evs = [ E_prev ev0; E_prev ev1 ] } in
   Imandrakit_twine.Encode.finalize_copy enc ~entrypoint:top
