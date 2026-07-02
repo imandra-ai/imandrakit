90f46527067890d917eec0bc76b8bbb217a85bfe
Christoph M. Wintersteiger <christoph@imandra.ai>
2026-07-02 19:10:36 +0000

    Fix tests

--- test/io/setrlimit/t2.ml
+++ test/io/setrlimit/t2.ml
@@ -2,7 +2,7 @@ module R = Imandrakit_io_setrlimit
 
 let () =
   (* 2 MB max *)
-  R.set_exn R.RLIMIT_AS 2_000_000;
+  R.set_hard_exn R.RLIMIT_AS (Some 2_000_000n);
   (* now allocate a 8MB array *)
   try
     let _arr = Sys.opaque_identity (Array.make 1_000_000 true) in
